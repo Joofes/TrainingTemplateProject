1a60810d1b874fd08f5ae76d28179f6a987b80bc
Joofes <64694259+Joofes@users.noreply.github.com>
2023-11-18 13:53:35 -0500

    Added simulated vars

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,13 +8,17 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 
 public class RobotContainer {
   private final DriveSubsystem m_drivetrain = new DriveSubsystem();
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-
+  
   public RobotContainer() {
     configureBindings();
   }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,15 +4,59 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  public DriveSubsystem() {}
+  public DriveSubsystem() {
+    leftEncoderSim.setDistancePerPulse(2 * Math.PI * 3 / 4096);
+    rightEncoderSim.setDistancePerPulse(2 * Math.PI * 3 / 4096);
+  }
+  
+  private PWMSparkMax leftMotor = new PWMSparkMax(0);
+  private PWMSparkMax rightMotor = new PWMSparkMax(1);
+
+  private Encoder l_Encoder = new Encoder(0, 1);
+  private Encoder r_Encoder = new Encoder(2, 3);
+    
+  private EncoderSim leftEncoderSim = new EncoderSim(l_Encoder);
+  private EncoderSim rightEncoderSim = new EncoderSim(r_Encoder);
+
+  private AnalogGyro gyro = new AnalogGyro(1);
+  private AnalogGyroSim gyroSim = new AnalogGyroSim(gyro);
+
+  private DifferentialDrivetrainSim drivetrainSim = DifferentialDrivetrainSim.createKitbotSim(
+  KitbotMotor.kDualCIMPerSide, // 2 CIMs per side.
+  KitbotGearing.k10p71,        // 10.71:1
+  KitbotWheelSize.kSixInch,    // 6" diameter wheels.
+  null           
+ );
+
+  private Field2d field = new Field2d();
+
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+      drivetrainSim.setInputs(leftMotor.get() * RobotController.getInputVoltage(), rightMotor.get() * RobotController.getInputVoltage());
+
+      drivetrainSim.update(.02);
+
+      leftEncoderSim.setDistance(drivetrainSim.getLeftPositionMeters());
+      leftEncoderSim.setRate(drivetrainSim.getLeftVelocityMetersPerSecond());
+      rightEncoderSim.setDistance(drivetrainSim.getRightPositionMeters());
+      rightEncoderSim.setRate(drivetrainSim.getRightVelocityMetersPerSecond());
+      gyroSim.setAngle(drivetrainSim.getHeading().getDegrees());
   }
 }
