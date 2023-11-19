c98086c0345b3ac5ee1dc2e4357aa9f0334622cf
Joofes <64694259+Joofes@users.noreply.github.com>
2023-11-19 09:33:16 -0500

    Revert "Revert "Continued progress""
    This reverts commit cb3ec252ae746c0add2406f5d4d90b7e8b55ce0a.

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,9 +4,13 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
@@ -15,7 +19,9 @@ import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -23,6 +29,8 @@ public class DriveSubsystem extends SubsystemBase {
   public DriveSubsystem() {
     leftEncoderSim.setDistancePerPulse(2 * Math.PI * 3 / 4096);
     rightEncoderSim.setDistancePerPulse(2 * Math.PI * 3 / 4096);
+
+    SmartDashboard.putData(field);
   }
   
   private PWMSparkMax leftMotor = new PWMSparkMax(0);
@@ -43,6 +51,13 @@ public class DriveSubsystem extends SubsystemBase {
   KitbotWheelSize.kSixInch,    // 6" diameter wheels.
   null           
  );
+  
+  private DifferentialDrive m_diffDrive  = new DifferentialDrive(leftMotor, rightMotor);
+
+ DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(
+  gyro.getRotation2d(),
+  l_Encoder.getDistance(), r_Encoder.getDistance()
+ );
 
   private Field2d field = new Field2d();
 
@@ -58,5 +73,49 @@ public class DriveSubsystem extends SubsystemBase {
       rightEncoderSim.setDistance(drivetrainSim.getRightPositionMeters());
       rightEncoderSim.setRate(drivetrainSim.getRightVelocityMetersPerSecond());
       gyroSim.setAngle(drivetrainSim.getHeading().getDegrees());
+
+      var gyroAngle = gyro.getRotation2d();
+
+      m_odometry.update(gyro.getRotation2d(),
+                    leftEncoderSim.getDistance(),
+                    rightEncoderSim.getDistance());
+    field.setRobotPose(m_odometry.getPoseMeters());
+     
+  }
+
+  public void forwardMove()
+  {
+    m_diffDrive.arcadeDrive(1, 0);
+  }
+
+  public InstantCommand fwrdCmd() {
+    return new InstantCommand(() -> forwardMove());
+  }
+
+  public void backwardMove()
+  {
+    m_diffDrive.arcadeDrive(-1, 0);
+  }
+
+  public InstantCommand bwrdCmd() {
+    return new InstantCommand(() -> backwardMove());
+  }
+
+  public void leftTurn()
+  {
+    m_diffDrive.arcadeDrive(0, 1);
+  }
+
+  public InstantCommand leftCmd() {
+    return new InstantCommand(() -> leftTurn());
+  }
+
+  public void rightTurn()
+  {
+    m_diffDrive.arcadeDrive(0, -1);
+  }
+
+  public InstantCommand rightCmd() {
+    return new InstantCommand(() -> rightTurn());
   }
 }
